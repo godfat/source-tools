6bb4fb4fb651af1f9e3b6fb309fc5b91b3027df4
godfat <godfat@godfat.org>
2009-01-13 03:38:41 +0800

    fixed missing | |

--- lib/source-tools.rb
+++ lib/source-tools.rb
@@ -15,7 +15,7 @@ module SourceTools
   def each_source_path
     require 'pathname'
 
-    Pathname.getwd.find{ path
+    Pathname.getwd.find{ |path|
       # skip unreadable, unwritable, .git and .svn directories
       skip_dir = path.directory? && (!path.readable? || !path.writable?)
       Find.prune if skip_dir || %w[.git .svn].include?(path.basename.to_s)
