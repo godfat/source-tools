afdd1fc0f14d4bde00929ac83b9f01838b11c7d1
Lin Jen-Shin <godfat@godfat.org>
2009-06-11 16:51:40 +0800

    update error message, add header comment for html_eof(eof??) task as well

--- lib/source-tools/tasks/html_eof.rb
+++ lib/source-tools/tasks/html_eof.rb
@@ -2,15 +2,22 @@
 namespace :st do
   desc 'Append a html comment at the end of file (for tracking partial template on generated html)'
   task :html_eof, :glob do |t, args|
-    raise ArgumentError.new('Please provide a glob to indicate which files you want to append the comment') unless args[:glob]
-    require 'pathname'
+    if args[:glob].nil?
+      puts 'Please provide a glob to indicate which files you want to append the comment'
+      exit(1)
+    end
 
+    require 'pathname'
     Pathname.glob(args[:glob]).each{ |path|
       next unless path.writable? && !path.directory?
 
-      path.open('a'){ |f|
+      content = path.read
+
+      path.open('w'){ |f|
         puts "Processing #{path}"
-        f.puts "<!-- #{path} -->"
+        f.puts "<!-- begin #{path} -->"
+        f.print content
+        f.puts "<!--   end #{path} -->"
       }
     }
 
