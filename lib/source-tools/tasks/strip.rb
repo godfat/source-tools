a8b6fefaac9934ee526d9432494676597e912c37
Lin Jen-Shin <godfat@godfat.org>
2009-11-02 15:23:48 +0800

    [strip.rb] now st:strip could take glob args

--- lib/source-tools/tasks/strip.rb
+++ lib/source-tools/tasks/strip.rb
@@ -3,20 +3,27 @@
 
 namespace :st do
   desc 'Strip trailing whitespace from source files, also normalize tabs to spaces, and an newline at end of file'
-  task :strip, :spaces do |t, args|
+  task :strip, :spaces, :glob do |t, args|
     require 'source-tools'
     require 'zlib'
 
     spaces = ' ' * (args[:spaces] || 2).to_i
 
-    SourceTools.each_source_path{ |path|
+    kind = if args[:glob]
+             [:glob, args[:glob]]
+           else
+             [:each_source_path]
+           end
+
+    SourceTools.send(*kind){ |path|
       # strat striping unnecessary whitespaces
       result = path.open('r:binary'){ |f| SourceTools.strip(f, spaces) }
 
       # skip the file if it was not modified
       next if Zlib.crc32(result) == Zlib.crc32(path.read)
 
-      puts "Stripping #{path.relative_path_from(Pathname.new(Dir.pwd))}"
+      puts "Stripping #{args[:glob] ? path
+                                    : path.relative_path_from(Pathname.new(Dir.pwd))}"
       path.open('w'){ |f| f.write result }
     }
 
